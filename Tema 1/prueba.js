ade887961ff120aa2d78811d8ba20cd9fcab8a71
AndresGS <agayososegui@iessonferrer.net>
2021-10-04 16:35:00 +0200

    js

--- Tema 1/prueba.js
+++ Tema 1/prueba.js
@@ -1,3 +1,14 @@
+/*mostrar por pantalla*/
 console.log("Hola desde un archivo externo");
+
+/*Declarar variables*/
+
+/*var defines la variable en un ambito global*/
 var x = 1;
 console.log(x);
+
+/*let defines la variable dentro del bloque (si lo intentamos mostrar fuera de los {} no funciona)*/
+{
+    let y = 2
+    console.log(y)
+}
