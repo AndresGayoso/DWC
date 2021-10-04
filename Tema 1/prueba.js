cd18bb2cecbadb39151f5ff1a5adc27d868b8af9
AndresGS <agayososegui@iessonferrer.net>
2021-10-04 16:49:13 +0200

    js

--- Tema 1/prueba.js
+++ Tema 1/prueba.js
@@ -1,14 +1,28 @@
-/*mostrar por pantalla*/
+/* mostrar por pantalla */
 console.log("Hola desde un archivo externo");
 
-/*Declarar variables*/
+/* Declarar variables */
 
-/*var defines la variable en un ambito global*/
+/* var defines la variable en un ambito global */
 var x = 1;
 console.log(x);
 
-/*let defines la variable dentro del bloque (si lo intentamos mostrar fuera de los {} no funciona)*/
+/* let defines la variable dentro del bloque */
 {
-    let y = 2
-    console.log(y)
+    let y = 2;
+    console.log(y);
 }
+/* Aqui fuera ya no funciona el console.log */
+
+/* typeof nos define el tipo de los datos que le insertamos */
+console.log(typeof(2));
+console.log(typeof(true));
+console.log(typeof("andres"));
+
+/* Tipos de declarar una variable */
+
+var nombre1 = "andres";
+var nombre2 = 'andres';
+var nombre3 = `andres`;
+
+console.log("Mi nombre es " + nombre1);
