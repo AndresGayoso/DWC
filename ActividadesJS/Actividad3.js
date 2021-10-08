17a1df0980b58a76ef41cf490b62a409f16a19be
AndresGS <agayososegui@iessonferrer.net>
2021-10-08 19:46:42 +0200

    js

--- ActividadesJS/Actividad3.js
+++ ActividadesJS/Actividad3.js
@@ -1,30 +1,29 @@
-function CalcularSalario(){
-
+function MostrarTexto(){
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let edad = parseInt(document.getElementById("edad").value);
     let salario = parseFloat(document.getElementById("salario").value);
 
     if(salario >= 1000 && salario <= 2000){
         if(edad > 45){
-
+            salario = salario + ((salario * 3) / 100);
         }
         if(edad <= 45){
-            
+            salario = salario + ((salario * 10) / 100);
         }
-
     }
 
+    if(salario < 1000){
+        if(edad < 30){
+            salario = 1100
+        }
+        if(edad >= 30 && edad <=45){
+            salario = salario + ((salario * 3) / 100);
+        }
+        if(edad > 45){
+            salario = salario + ((salario * 15) / 100);
+        }
+    }
 
-
-
-}
-
-
-
-
-
-function MostrarTexto(){
-    let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    var edad = parseInt(document.getElementById("edad").value);
-    let salario = CalcularSalario(); 
     document.write(nombre," ",apellido," tiene un salario de ",salario," con ",edad," años.");
 }
