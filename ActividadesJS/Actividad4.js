fbef8018b7d4be226688962dff339a8e72c76bc1
Andres GS <andresitooctubre@gmail.com>
2021-10-09 02:41:59 +0200

    js

--- ActividadesJS/Actividad4.js
+++ ActividadesJS/Actividad4.js
@@ -1,27 +1,29 @@
 var randomNumber = Math.floor(Math.random() * 100) + 1;
 var intentos = 0;
+var acabar = false;
+var numero;
 
-
-for(let i = 0; acabar == true;i++){
-    var numero = prompt("Adivina el numero",randomNumber);
+while(!acabar){
+    
+    numero = prompt("Adivina el numero",randomNumber);
     
     if(numero == randomNumber){
-        alert("Enhorabueno has acertado en: ",intentos);
+        alert("Enhorabueno has acertado en: " + intentos);
         acabar = true;
-        break;
-    }
-
-    if(numero < randomNumber){
-        alert("El numero es mas bajo");
         intentos++;
     }
 
     if(numero < randomNumber){
+        alert("El numero es mas alto");
+        intentos++;
+    }
+
+    if(numero > randomNumber){
         alert("El numero es mas bajo.");
         intentos++;
     }
 
-    if(!isNaN(numero)){
+    if(isNaN(numero)){
         alert("Eso no es un numero.");
     }
 
