3ecb2ac5b3489873073d2e6ce3db92c23b22c17f
AndresGS <agayososegui@iessonferrer.net>
2021-10-18 15:27:48 +0200

    js

--- ActividadesJS/Actividad8.js
+++ ActividadesJS/Actividad8.js
@@ -1,11 +1,14 @@
+//Funcion que crea la tabla de los unicode
 function CrearTabla(){
 
+    //Crear la tabla con los estilos
      document.write('<table style="border: 2px solid black">');
 
-            
+        //Bucle que nos saca los unicode con su numero
         for(let i =0;i<1000;i++){
             var simbolo = `&#`+i;
             document.write('<tr>');
+          //Bucle que nos muestra los unicode con su numero al lado
           for(let j=0;j<1;j++){
             document.write('<td style="border: 2px solid black">'+i+'</td>');
             document.write('<td style="border: 2px solid black">'+simbolo+'</td>');
@@ -15,4 +18,5 @@ function CrearTabla(){
         }
     document.write('</table>');
     }
+    //Ejecutar la funcion
     window.onload= CrearTabla();
