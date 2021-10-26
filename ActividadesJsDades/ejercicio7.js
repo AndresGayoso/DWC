5a0cb832e5c7918c415010044eea82f3c7124938
Andres GS <andresitooctubre@gmail.com>
2021-10-27 01:03:02 +0200

    js

--- ActividadesJsDades/ejercicio7.js
+++ ActividadesJsDades/ejercicio7.js
@@ -1,4 +1,40 @@
 function Palindromo(){
-    document.write("hola")
+    
+    var texto = prompt("Escribe una frase");
+
+    var minusculas = texto.toLocaleLowerCase();
+
+    var cadenaConEspacios = minusculas.split("");
+
+    var cadenaSinEspacios = "";
+
+     for(i in cadenaConEspacios) {
+        if(cadenaConEspacios[i] != " ") {
+            cadenaSinEspacios += cadenaConEspacios[i];
+        }
+    }
+
+    var cadena = cadenaSinEspacios.split("");
+
+    var cadenaAlReves = cadenaSinEspacios.split("").reverse();
+
+    var palindromo = false;
+
+    for(i in cadena){
+        if(cadena[i] == cadenaAlReves[i]){
+            palindromo = true;
+        }else{
+            document.write("No es un palindromo");
+            break;
+        }
+    }
+
+    if(palindromo == true){
+        document.write(texto,"<br>")
+        document.write("Es un palindromo");
+    }
+
+
+
 }
 window.onload = Palindromo();
