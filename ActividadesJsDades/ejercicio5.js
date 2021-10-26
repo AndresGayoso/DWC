992915bcd425cd93d2212159baee2da3c237320c
Andres GS <andresitooctubre@gmail.com>
2021-10-27 00:32:30 +0200

    js

--- ActividadesJsDades/ejercicio5.js
+++ ActividadesJsDades/ejercicio5.js
@@ -7,19 +7,13 @@ function verificacion(){
     var passwd;
 
     //variables para ver si se cumplen los requisitos
-    var minuscula = false;
-    var mayuscula = false;
-    var numero = false;
-    var simbolo = false
+    var comprobar = 0;
 
     //Variables para verificar
     var mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
     var minusculas = "abcdefghijklmnñopqrstuvwxyz";
     var numeros = "0123456789";
-    var simbolo = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789"
-
-    //Contador
-    var x = 0;
+    var simbolos = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789"
 
     //Nombre de usuario
     var usuario = prompt("Escribe el nombre de usuario");
@@ -40,44 +34,35 @@ function verificacion(){
             verificar = true;
         }       
 
-        bucle1:
         for(let i = 0; i < passwd.length;i++){
-            for(let x = 0; x < mayusculas.length;x++){
-                if(passwd.charAt(i) == mayusculas.charAt(x)){
-                    mayuscula = true;
-                    break bucle1;
-                }
-            }
-        }
-
-        bucle2:
-        for(let i = 0; i < passwd.length;i++){
-            for(let x = 0; x < minusculas.length;x++){
-                if(passwd.charAt(i) == minusculas.charAt(x)){
-                    minuscula = true;
-                    break bucle2;
-                }
-            }
-        }
-
-        bucle3:
-        for(let i = 0; i < passwd.length;i++){
-            for(let x = 0; x < numeros.length;x++){
-                if(passwd.charAt(i) == numeros.charAt(x)){
-                    numero = true;
-                    break bucle3;
-                }
-            }
+            if(minusculas.indexOf(passwd.charAt(i)) != -1 ){
+                comprobar++;
+                break;
+            } 
         }
 
         for(let i = 0; i < passwd.length;i++){
-            if(simbolo.indexOf(passwd.charAt(i)) == -1 ){
-                simbolo = true;
+            if(mayusculas.indexOf(passwd.charAt(i)) != -1 ){
+                comprobar++;
+                break;
+            } 
+        }
+
+        for(let i = 0; i < passwd.length;i++){
+            if(numeros.indexOf(passwd.charAt(i)) != -1 ){
+                comprobar++;
+                break;
+            } 
+        }
+
+        for(let i = 0; i < passwd.length;i++){
+            if(simbolos.indexOf(passwd.charAt(i)) == -1 ){
+                comprobar ++;
                 break;
             }            
         }
 
-        if(minuscula == true && mayuscula == true && numero == true && simbolo == true){
+        if(comprobar == 4){
             document.write("Usuario: ",usuario);
             document.write("<br>Contraseña: ",passwd);
             document.write("<br>Los datos se han almacenado correctamente");
