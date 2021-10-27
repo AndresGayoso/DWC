1ce00ec300fd2e774940c84b8173dd26e3667e0e
AndresGS <agayososegui@iessonferrer.net>
2021-10-27 16:54:14 +0200

    js

--- ActividadesJsDades/ejercicio8.js
+++ ActividadesJsDades/ejercicio8.js
@@ -1,29 +1,38 @@
 function MediaNumeros(){
 
+    //Variable para sumar los numero
     var numeros = 0;
     
+    //Donde se introduce el numero
     var numero;
 
+    //Finalizar el programa
     var acabar = false;
 
+    //Variable contador
     var contador = 0;
 
+    //Bucle que hara la media de los numeros
     while(!acabar){
         
+        //Introducir el numero
         numero = prompt("Escribe un numero(Si quieres acabar escribe 0)");
 
+        //Si cancela se acaba el programa
         if(numero == null){
             document.write("El programa se ha acabado y no se mostrara la media");
             acabar = true;
             break;
         }
 
+        //Si escribe 0 se acaba el programa y hace la media
         if(numero == 0){
             document.write("La media es: ",numeros / contador);
             acabar = true;
             break;
         }
 
+        //Si es un numero los suma al total(sino no se almacena)
         if(!isNaN(numero)){
             numeros += parseInt(numero);
             contador++;
