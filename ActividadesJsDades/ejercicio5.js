955002c082906ecb2c7c4fae967b4baac58f014a
AndresGS <agayososegui@iessonferrer.net>
2021-10-29 17:01:06 +0200

    js

--- ActividadesJsDades/ejercicio5.js
+++ ActividadesJsDades/ejercicio5.js
@@ -2,12 +2,13 @@ function verificacion(){
 
     //Variable para que acabe el bucle
     var verificar = false;
+    var verificar2 = false;
 
     //Contraseña
     var passwd;
 
     //variables para ver si se cumplen los requisitos
-    var comprobar = 0;
+    var comprobar;
 
     //Variables para verificar
     var mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
@@ -15,17 +16,44 @@ function verificacion(){
     var numeros = "0123456789";
     var simbolos = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789"
 
-    //Nombre de usuario
-    var usuario = prompt("Escribe el nombre de usuario");
+    while(!verificar){
 
-    //Cancela el programa
-    if(usuario == null){
-        document.write("Se ha acabado el programa")
-        verificar = true;
+        comprobar = 0;
+
+        var usuario = prompt("Escribe el nombre de usuario");
+
+        if (usuario == null){
+            document.write("Se ha acabado el programa");
+            verificar = true;
+        } 
+
+        for(let i = 0; i < usuario.length;i++){
+            if(mayusculas.indexOf(usuario.charAt(i)) != -1 ){
+                comprobar++;
+                break;
+            } 
+        }
+
+        for(let i = 0; i < usuario.length;i++){
+            if(simbolos.indexOf(usuario.charAt(i)) == -1 ){
+                comprobar ++;
+                break;
+            }            
+        }
+
+        if(comprobar == 0){
+            verificar = true;
+        }
+
+
     }
 
+   
+
     //Bucle para verificar la contraseña
-    while(verificar == false){
+    while(!verificar2){
+
+        comprobar = 0;
 
         //Escribimos la contraseña
         passwd = prompt("Escribe una contraseña");
@@ -33,7 +61,7 @@ function verificacion(){
         //Cancela el programa
         if (passwd == null){
             document.write("Se ha acabado el programa");
-            verificar = true;
+            verificar2 = true;
         }       
 
         //Verificar minusculas
@@ -73,7 +101,7 @@ function verificacion(){
             document.write("Usuario: ",usuario);
             document.write("<br>Contraseña: ",passwd);
             document.write("<br>Los datos se han almacenado correctamente");
-            verificar = true;
+            verificar2 = true;
         }
 
     }
