33d59f0870165d3fb8264f046b4b19b02710f0be
AndresGS <agayososegui@iessonferrer.net>
2021-11-02 19:57:22 +0100

    js

--- ActividadesJSFunciones/ejercicio1.js
+++ ActividadesJSFunciones/ejercicio1.js
@@ -1,19 +1,55 @@
-function Escribir(){
-    var numero;
+var numero;
+
+//Funcion Dlecarada
+function Escribir1(){
 
     for(let i = 0;i<500;i++){
         numero = Math.floor(Math.random() * 10000);
-        document.write(numero,ParImpar(numero),"<br>");
+        document.write(numero,ParImpar(),"<br>");
     }
 
 }
 
-function ParImpar(num){
-    if(num % 2 == 0){
+function ParImpar(){
+    if(numero % 2 == 0){
         return " es par";
     }else{
         return " es impar";
     }
 }
 
-window.onload = Escribir();
+//Funcion Anonima
+function Escribir2(){
+
+    for(let i = 0;i<500;i++){
+        numero = Math.floor(Math.random() * 10000);
+        document.write(numero,Comprovar(),"<br>");
+    }
+
+}
+
+const Comprovar = function ParImpar(){
+    if(numero % 2 == 0){
+        return " es par";
+    }else{
+        return " es impar";
+    }
+}
+
+//Funcion Flechitas
+function Escribir3(){
+
+    for(let i = 0;i<500;i++){
+        numero = Math.floor(Math.random() * 10000);
+        document.write(numero,parImpar(),"<br>");
+    }
+
+}
+
+const parImpar = () =>{
+    if(numero % 2 == 0){
+        return " es par";
+    }else{
+        return " es impar";
+    }
+}
