d29f3ed101ef655645faf038ceb67f8af177c3f5
AndresGS <agayososegui@iessonferrer.net>
2021-11-03 20:48:59 +0100

    js

---
+++ ActividadesJSFunciones/ejercicio2.js
@@ -0,0 +1,12 @@
+var fila;
+var columna;
+var color;
+
+function Tabla1(){
+
+    document.write('<table border="1" style="backgorund-color:' + color +';border-collapse: collapse;width:100%;height:15%;">');
+    for(let i = 0; )
+    document.write("</table>");
+
+
+}
