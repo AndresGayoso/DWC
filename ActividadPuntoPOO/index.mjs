75fcc88f5758d5d453c525d7c38bf09a04e549b8
AndresGS <agayososegui@iessonferrer.net>
2021-11-17 20:56:35 +0100

    js

--- ActividadPuntoPOO/index.mjs
+++ ActividadPuntoPOO/index.mjs
@@ -1,4 +1,15 @@
 import {Punto} from "./punto.mjs";
 
 var p = new Punto(1,2);
-console.log(p.X + " " + p.Y);
+console.log("p: " + p.toString())
+var q = new Punto(6,-3);
+console.log("q: " + q.toString());
+
+p.cambiar(-5,2);
+
+var r = p.copia();
+console.log("r: " + r.toString());
+r.X = 9;
+
+console.log("p: " + p.toString());
+console.log("r: " + r.toString());
--- ActividadPuntoPOO/punto.mjs
+++ ActividadPuntoPOO/punto.mjs
@@ -1,5 +1,20 @@
-function Punto(CoordX,CoordY){
-    this.X = CoordX;
-    this.Y = CoordY;
+class Punto {
+    constructor(CoordX, CoordY) {
+        this.X = CoordX;
+        this.Y = CoordY;
+    }
+    cambiar(NX,NY){
+        this.X = NX;
+        this.Y = NY;
+    }
+    toString(){
+        return "(" + this.X + "," + this.Y + ")";
+    }
+    copia(){
+        return new Punto(this.X,this.Y)
+    }
+    x(){
+        this.X = NX;
+    }
 }
 export {Punto};
