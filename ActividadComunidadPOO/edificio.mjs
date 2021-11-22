69df9a4835720bd7da0d68f3ca628760a50cc4dc
AndresGS <agayososegui@iessonferrer.net>
2021-11-22 19:00:54 +0100

    js

--- ActividadComunidadPOO/edificio.mjs
+++ ActividadComunidadPOO/edificio.mjs
@@ -1,20 +1,47 @@
-class Edificio{
-    constructor(tipoVia, nombreVia, numeroEdificio, codigoPostal){
+function Edificio(tipoVia, nombreVia, numeroEdificio, codigoPostal){
         this.tipoVia = tipoVia;
         this.nombreVia = nombreVia;
         this.numeroEdificio = numeroEdificio;
         this.codigoPostal = codigoPostal;
         this.mapaPropietariosEdificio = new Map();
-    }
-    agregarPlanta(numeroPlanta){
-        this.mapaPropietariosEdificio.set(numeroPlanta,this.numeroPlanta = new Map());
-    }
-    agregarPuerta(numeroPlanta,numeroPuerta){
-        this.numeroPlanta.set(numeroPlanta,numeroPuerta = []);
-    }
-    agregarPropietario(nombrePropietario, numeroPlanta, numeroPuerta){
-      
-    }
 }
+
+Edificio.prototype.agregarPlanta = function (numeroPlanta) {
+    let mapaPlanta = new Map();
+    this.mapaPropietariosEdificio.set(numeroPlanta,mapaPlanta);
+}
+
+Edificio.prototype.agregarPuerta = function (numeroPlanta,numeroPuerta){
+    this.mapaPropietariosEdificio.get(numeroPlanta).set(numeroPuerta,[]);
+}
+
+Edificio.prototype.agregarPropietario = function (nombrePropietario,numeroPlanta,numeroPuerta){
+    this.mapaPropietariosEdificio.get(numeroPlanta).get(numeroPuerta).push(nombrePropietario);
+}
+
+Edificio.prototype.imprimirTodosPropietarios = function (){
+    let KeyPlanta = this.mapaPropietariosEdificio.keys();
+    let Planta;
+    let KeyPuerta;
+    let Puerta;
+    let Propietario;
+    let resultado = "";
+    /*let puertas = this.mapaPropietariosEdificio.get().value*/
+   for(let i = 0; i < this.mapaPropietariosEdificio.size; i++){
+        Planta = KeyPlanta.next().value;
+        KeyPuerta = this.mapaPropietariosEdificio.get(Planta).keys()
+        resultado += "Planta: " + Planta + "\n";
+        for(let x = 0; x < this.mapaPropietariosEdificio.get(Planta).size;x++){
+            Puerta = KeyPuerta.next().value
+            resultado += "\t" + "Puerta: " + Puerta + "\n";
+            for(let j = 0; j < this.mapaPropietariosEdificio.get(Planta).get(Puerta).length;j++){
+                Propietario = this.mapaPropietariosEdificio.get(Planta).get(Puerta);
+                resultado += "\t\t" + Propietario[j] + "\n";
+            }
+        }
+    }
+    return resultado;
+}
+
 
 export {Edificio};
--- ActividadComunidadPOO/index.mjs
+++ ActividadComunidadPOO/index.mjs
@@ -3,6 +3,7 @@ import { Edificio } from "./edificio.mjs";
 const edificio1 = new Edificio('calle', 'marques de la fontsanta', 60, '07005');
 console.log(edificio1)
 
+
 edificio1.agregarPlanta('1A');
 edificio1.agregarPlanta('1B');
 console.log(edificio1);
@@ -10,5 +11,13 @@ console.log(edificio1);
 
 edificio1.agregarPuerta('1A','3');
 edificio1.agregarPuerta('1A','2');
-edificio1.agregarPuerta('1B','1')
+edificio1.agregarPuerta('1B','1');
 console.log(edificio1);
+
+edificio1.agregarPropietario('Belén Laserna Belenguer','1A','3');
+edificio1.agregarPropietario('Pedro Jimenez Vázquez','1A','3');
+edificio1.agregarPropietario('Ana María Rodriguez Figuerola', '1A','2');
+edificio1.agregarPropietario('Miguel López López','1B','1');
+console.log(edificio1)
+
+console.log(edificio1.imprimirTodosPropietarios())
