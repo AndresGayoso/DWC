ce45234d861fde8950e7522ef248d2b0a9e46287
AndresGS <agayososegui@iessonferrer.net>
2021-11-26 15:35:00 +0100

    js

--- ActividadComunidadPOO/Actividad2/json.js
+++ ActividadComunidadPOO/Actividad2/json.js
@@ -16,6 +16,7 @@ var edificio1 = `{
         }
     }
 }`;
+
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -28,40 +29,3 @@ function getRandomColor() {
    function setRandomColor() {
        document.body.style.backgroundColor = getRandomColor();
    }
-/*
-var json = JSON.parse(edificio1);
-
-var edificio = new Edificio(json.tipoVia,json.nombreVia,json.numeroEdificio,json.codigoPostal);
-
-console.log(edificio);
-
-console.log(edificio.imprimirTipoVia());
-console.log(edificio.imprimirNombreVia());
-console.log(edificio.imprimirNumeroEdificio());
-console.log(edificio.imprimirCodigoPostal());
-
-
-var objeto = JSON.parse(edificio1);
-
-console.log(objeto);
-
-var array = Object.entries(objeto.mapaPropietariosEdificio);
-
-console.log(array);
-
-var array2;
-
-for(let i = 0; i < array.length;i++){
-    edificio.agregarPlanta(array[i][0])
-    array2 = Object.entries(array[i][1])
-    for(let j = 0; j < array2.length;j++){
-        edificio.agregarPuerta(array[i][0],array2[j][0])
-        for(let z = 0; z < array2[j][1].length;z++){
-            edificio.agregarPropietario(array2[j][1][z],array[i][0],array2[j][0])
-        }
-    }
-}
-
-var x = edificio.imprimirTodosPropietarios()
-console.log(x)
-*/
