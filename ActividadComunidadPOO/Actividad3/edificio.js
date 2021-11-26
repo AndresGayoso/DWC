2909bbb4e459adbbac4a4f57abb2af74d2410fb9
Andres GS <andresitooctubre@gmail.com>
2021-11-26 23:19:01 +0100

    js

--- ActividadComunidadPOO/Actividad3/edificio.js
+++ ActividadComunidadPOO/Actividad3/edificio.js
@@ -1,76 +1,88 @@
+//Constructor
 function Edificio(tipoVia, nombreVia, numeroEdificio, codigoPostal){
-        this.tipoVia = tipoVia;
-        this.nombreVia = nombreVia;
-        this.numeroEdificio = numeroEdificio;
-        this.codigoPostal = codigoPostal;
-        this.mapaPropietariosEdificio = new Map();
+    this.tipoVia = tipoVia;
+    this.nombreVia = nombreVia;
+    this.numeroEdificio = numeroEdificio;
+    this.codigoPostal = codigoPostal;
+    this.mapaPropietariosEdificio = new Map();
 }
 
+//Agregar la planta al edificio en el mapa
 Edificio.prototype.agregarPlanta = function (numeroPlanta) {
-    let mapaPlanta = new Map();
-    this.mapaPropietariosEdificio.set(numeroPlanta,mapaPlanta);
+let mapaPlanta = new Map();
+this.mapaPropietariosEdificio.set(numeroPlanta,mapaPlanta);
 }
 
+//Agregar la puerta a la planta en el mapa
 Edificio.prototype.agregarPuerta = function (numeroPlanta,numeroPuerta){
-    this.mapaPropietariosEdificio.get(numeroPlanta).set(numeroPuerta,[]);
+this.mapaPropietariosEdificio.get(numeroPlanta).set(numeroPuerta,[]);
 }
 
+//Agregar los propietarios a su puerta y planta en el mapa
 Edificio.prototype.agregarPropietario = function (nombrePropietario,numeroPlanta,numeroPuerta){
-    this.mapaPropietariosEdificio.get(numeroPlanta).get(numeroPuerta).push(nombrePropietario);
+this.mapaPropietariosEdificio.get(numeroPlanta).get(numeroPuerta).push(nombrePropietario);
 }
 
+//Retorna un string con todos los propietarios asignados a su puerta y planta
 Edificio.prototype.imprimirTodosPropietarios = function (){
-    let KeyPlanta = this.mapaPropietariosEdificio.keys();
-    let Planta;
-    let KeyPuerta;
-    let Puerta;
-    let Propietario;
-    let resultado = "";
-    let tab = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-   for(let i = 0; i < this.mapaPropietariosEdificio.size; i++){
-        Planta = KeyPlanta.next().value;
-        KeyPuerta = this.mapaPropietariosEdificio.get(Planta).keys()
-        resultado += '<h2 class="puerta">Planta: ' + Planta + "<br></h2>";
-        for(let x = 0; x < this.mapaPropietariosEdificio.get(Planta).size;x++){
-            Puerta = KeyPuerta.next().value
-            resultado += "<h3>" + tab + "Puerta: " + Puerta + "<br></h3>";
-            for(let j = 0; j < this.mapaPropietariosEdificio.get(Planta).get(Puerta).length;j++){
-                Propietario = this.mapaPropietariosEdificio.get(Planta).get(Puerta);
-                resultado += "<p>"+tab+tab + Propietario[j] + "<br></p>";
-            }
+let KeyPlanta = this.mapaPropietariosEdificio.keys();
+let Planta;
+let KeyPuerta;
+let Puerta;
+let Propietario;
+let resultado = "";
+for(let i = 0; i < this.mapaPropietariosEdificio.size; i++){
+    Planta = KeyPlanta.next().value;
+    KeyPuerta = this.mapaPropietariosEdificio.get(Planta).keys()
+    resultado += "Planta: " + Planta + " \n";
+    for(let x = 0; x < this.mapaPropietariosEdificio.get(Planta).size;x++){
+        Puerta = KeyPuerta.next().value
+        resultado += "\t" + "Puerta: " + Puerta + " \n";
+        for(let j = 0; j < this.mapaPropietariosEdificio.get(Planta).get(Puerta).length;j++){
+            Propietario = this.mapaPropietariosEdificio.get(Planta).get(Puerta);
+            resultado += "\t\t" + Propietario[j] + " \n";
         }
     }
-    return resultado;
+}
+return resultado;
 }
 
+//Retorna string con el tipo de via
 Edificio.prototype.imprimirTipoVia = function (){
-    return "Tipo de via: " + this.tipoVia;
+return "Tipo de via: " + this.tipoVia;
 }
 
+//Retorna string con el nombre de la via
 Edificio.prototype.imprimirNombreVia = function (){
-    return "Nombre de la via: " + this.nombreVia;
+return "Nombre de la via: " + this.nombreVia;
 }
 
+//Retorna string con el numero del edificio
 Edificio.prototype.imprimirNumeroEdificio = function (){
-    return "Numero del edificio: " + this.numeroEdificio;
+return "Numero del edificio: " + this.numeroEdificio;
 }
 
+//Retorna string con el codigo postal
 Edificio.prototype.imprimirCodigoPostal = function (){
-    return "Codigo Postal (CP): " + this.codigoPostal;
+return "Codigo Postal (CP): " + this.codigoPostal;
 }
 
+//Modificamos el tipo de via
 Edificio.prototype.modificarTipoVia = function (TipoVia){
-    this.TipoVia = TipoVia;
+this.TipoVia = TipoVia;
 }
 
+//Modificamos el nombre de la via
 Edificio.prototype.modificarNombreVia = function (nombreVia){
-    this.nombreVia = nombreVia;
+this.nombreVia = nombreVia;
 }
 
+//Modificamos el numero del edificio 
 Edificio.prototype.modificarNumeroEdificio = function (numeroEdificio){
-    this.numeroEdificio = numeroEdificio;
+this.numeroEdificio = numeroEdificio;
 }
 
+//Modificamos el codigo postal
 Edificio.prototype.modificarCodigoPostal = function (codigoPostal){
-    this.codigoPostal = codigoPostal;
+this.codigoPostal = codigoPostal;
 }
--- ActividadComunidadPOO/Actividad3/funciones.js
+++ ActividadComunidadPOO/Actividad3/funciones.js
@@ -1,7 +1,49 @@
+function CrearObjeto() {
+    var tipoVia;
+    var nombreVia;
+    var numeroEdificio;
+    var codigoPostal;
 
+    while (tipoVia == null) {
+        tipoVia = prompt("Introduce el tipo de via:");
+        if (tipoVia != null) {
+            alert("El dato se ha introducido correctamente");
+        } else if (tipoVia == null) {
+            alert("Debes introducir un dato");
+        }
+    }
 
+    while (nombreVia == null) {
+        nombreVia = prompt("Introduce el nombre de la via: ");
+        if (nombreVia != null) {
+            alert("El dato se ha introducido correctamente");
+        } else if (nombreVia == null) {
+            alert("Debes introducir un dato");
+        }
+    }
 
+    while (numeroEdificio == null) {
+        numeroEdificio = prompt("Introduce el numero del edificio: ");
+        if (numeroEdificio != null) {
+            alert("El dato se ha introducido correctamente");
+        } else if (numeroEdificio == null) {
+            alert("Debes introducir un dato");
+        }
+    }
 
+    while (codigoPostal == null) {
+        codigoPostal = prompt("Introduce el codigo postal: ");
+        if (codigoPostal != null) {
+            alert("El dato se ha introducido correctamente");
+        } else if (codigoPostal == null) {
+            alert("Debes introducir un dato");
+        }
+    }
+
+    var edificio = new Edificio(tipoVia, nombreVia, numeroEdificio, codigoPostal);
+
+    return edificio;
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
