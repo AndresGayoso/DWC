da7ce87d57dd256b40f297d978115fb7f9d28cd2
AndresGS <agayososegui@iessonferrer.net>
2021-12-01 18:36:24 +0100

    js

--- ActividadJSURL/index.js
+++ ActividadJSURL/index.js
@@ -2,11 +2,15 @@ function Validar(){
 
     var url = prompt("Escribe la url: ");
 
-    condicion = /http:\/{0,3}([a-zA-Z0-9-.]+:?([\w][\W])*) | ([a-zA-Z0-9-.])+ | ([\w]+[\W]*)@$/
+    condicion = /^(http|https|ftp):\/{0,3}(([a-zA-Z0-9-.])+(:[\w]+[\W]*) | ([a-zA-Z0-9-.])+ | ([\w]+[\W]*))@$/
 
-    texto = /([a-z] | [0-9]):$/
+    //texto = /([a-zA-Z0-9-.]+:?([\w][\W])* | ([a-zA-Z0-9-.])+ | ([\w][\W])*):$/
 
     //([a-z][A-Z][0-9])*
+
+    //texto = /[a-zA-Z0-9-.]+:([\w][\W])*:$/
+
+    texto = /([a-zA-Z0-9-.])+(:[\w]+[\W]*):$/
 
     if(condicion.test(url) == true){
         document.write("t")
