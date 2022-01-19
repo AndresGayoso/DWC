30290db056afcbfb25c426db3a209542fd76ad4a
AndresGS <agayososegui@iessonferrer.net>
2022-01-19 19:59:08 +0100

    js

--- Examen_Recu_POO/Ejercicio3/carritoCompra.js
+++ Examen_Recu_POO/Ejercicio3/carritoCompra.js
@@ -2,19 +2,49 @@ function CarritoCompra() {
     this.carrito = new Array();
 
     
-    function localizarItemCarrito(producto) {
-        // AÑADIR CÓDIGO AQUÍ
+    this.localizarItemCarrito = function(producto) {
+        for(let i = 0; i < this.carrito.length;i++){
+            if(this.carrito[i][0] == producto){
+                return i
+            }
+        }
+        return -1
     }   
 
-    this.introducirItemCarrito =function (producto, cantidad) {
+    this.introducirItemCarrito = function (producto, cantidad) {
             this.carrito.push(new Array(producto, cantidad));
     }
 
     
     this.borrarItemCarrito = function (producto, cantidad) {
-        let posicionItemCarrito = localizarItemCarrito(producto);
-
+        let posicionItemCarrito = this.localizarItemCarrito(producto);
+        this.carrito[posicionItemCarrito][1] -= cantidad;
+        if(this.carrito[posicionItemCarrito][1] <= 0){
+            this.carrito.splice(posicionItemCarrito,1);
+        }
         // AÑADIR CÓDIGO AQUÍ
     }
 
 }
+
+/*Pruebas
+
+//Crear objeto
+var carrito = new CarritoCompra();
+
+//Introducir valores
+carrito.introducirItemCarrito("salsa",4)
+carrito.introducirItemCarrito("cola",7)
+
+//Localizar items
+console.log(carrito.localizarItemCarrito("sal"))//mal
+console.log(carrito.localizarItemCarrito("cola"))//bien
+
+//Borrar item parcial
+carrito.borrarItemCarrito("cola",3)
+console.log(carrito)
+
+//Borrar item completo
+carrito.borrarItemCarrito("cola",4)
+console.log(carrito)
+*/
