ce7f3fa5679c6f34f1351e91cab98a58021aa1da
AndresGS <agayososegui@iessonferrer.net>
2022-02-07 17:52:42 +0100

    js

---
+++ ActividadTablasDOM/acciones.js
@@ -0,0 +1,25 @@
+function crearTablaColores(tablaColores,numColores){
+
+    var colores = document.getElementById(numColores).value
+    
+    if(colores >= 1 && colores <=20){
+
+    }
+
+}
+
+function crearTabla(numFila,NumColumna){
+
+}
+
+function introducirDatosTabla(tabla,datos){
+
+}
+
+function permutarfilas(tablaColores,fila1,fila2){
+
+}
+
+function cambiarFondo(tablaColores,filaFondo){
+
+}
