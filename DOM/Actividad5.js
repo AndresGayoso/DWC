7c85a3afd82740cfede3d50c29cd76aeec6bd732
AndresGS <agayososegui@iessonferrer.net>
2022-02-14 18:49:36 +0100

    js

--- DOM/Actividad5.js
+++ DOM/Actividad5.js
@@ -1,19 +1,16 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    let textarea = document.getElementById('areatexto');
-    let nodoParrafo = document.createElement('p');
-    nodoParrafo.id = 'parrafoCaracteres'
-    let nodoTextoParrafo = document.createTextNode(`Pots escriure ${textarea.maxLength - textarea.value.length} caràcters`)
-    nodoParrafo.appendChild(nodoTextoParrafo);
-    document.getElementById('areaFormulario').appendChild(nodoParrafo)
-});
+function OrdenarInversa(){
+    
+    let parrafos = document.getElementsByTagName("p");
 
-function insertarNumeroCaracteres(parrafo,texto){
-    let p = document.getElementById(parrafo)
-    let numeroCaracteres = document.getElementById(texto).value.length
-    let maxCaracteres = document.getElementById(texto).getAttribute("maxlength")
-    let restantes = maxCaracteres - numeroCaracteres
+    let arrayParrafos = Array.from(parrafos);
 
-    p.innerHTML = `Pots escriure ${restantes} caràcters`
+    let invertir = arrayParrafos.reverse();
+
+    let body = document.getElementsByTagName("body")[0]
+
+    for (let i = 0; i < invertir.length;i++){
+          body.appendChild(invertir[i])  
+          console.log(invertir[i])
+    }   
+
 }
-
-
